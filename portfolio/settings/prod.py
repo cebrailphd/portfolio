6699dd40f416f72ef6395cc98daf72021f4ff0d3
Cebrail <cebrail.puer@gmail.com>
2023-01-31 12:19:01 +0100

    allowed hosts

--- portfolio/settings/prod.py
+++ portfolio/settings/prod.py
@@ -4,5 +4,5 @@ import os
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['cebo.pythonanywhere.com','127.0.0.1','localhost']
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
